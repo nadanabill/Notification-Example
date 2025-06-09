440a2ae48173c7dacb46ff65e25b56e2350c6652
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-06-10 00:48:17 +0300

    test local notification in button

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/services/notification_service.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(
+        child: TextButton(
+          onPressed: () async {
+            await NotificationService().showNotification(
+              id: '1',
+              title: 'Notification Title',
+              body: 'Notification Body',
+            );
+          },
+          child: const Text('Show Notification'),
+        ),
+      ),
+    );
   }
 }

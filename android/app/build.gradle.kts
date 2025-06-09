d3bab4c0219ead5e00a0a9e7a73fb7cdb768ef59
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-06-10 00:28:29 +0300

    updated compileSdk to 35 and ndkVersion to "27.0.12077973"

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,10 +7,11 @@ plugins {
 
 android {
     namespace = "com.example.notification_example"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -25,7 +26,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }

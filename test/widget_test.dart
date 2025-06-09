66665a0965d3dcf621d25014770838226ec385d1
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-06-09 07:40:38 +0300

    handled app files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:notification_example/main.dart';
+import 'package:notification_example/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

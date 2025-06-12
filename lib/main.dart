6369c2ad6332f4685d7704231071c8313afc5e38
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-06-12 07:36:09 +0300

    added firebase cloud messaging service

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'core/services/firebase_cloud_messaging_service.dart';
 import 'core/services/notification_service.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize local notifications
   await NotificationService.initialize();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Initialize FCM service
+  await FirebaseCloudMessagingService.initialize();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
 }
